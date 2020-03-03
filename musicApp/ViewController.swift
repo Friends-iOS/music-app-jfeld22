8f39b42a22ef89c8c259b9b95b8fa0c985dcd917
Joshua <22feld@friendsseminary.org>
2020-03-02 22:11:19 -0500

    hw march 3

--- musicApp/ViewController.swift
+++ musicApp/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func playSound(name : String) {
+        let sound = Bundle.main.path(forResource: name, ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
     }
 
-
+    @IBAction func playC(_ sender: Any) {
+        playSound(name : "c-4")
+    }
+    
+    @IBAction func playD(_ sender: Any) {
+        playSound(name : "d-4")
+    }
+    
+    @IBAction func playE(_ sender: Any) {
+        playSound(name : "e4")
+    }
+    
+    @IBAction func playF(_ sender: Any) {
+        playSound(name : "f-4")
+    }
+    
+    @IBAction func playG(_ sender: Any) {
+        playSound(name : "g-4")
+    }
+    
+    @IBAction func playA(_ sender: Any) {
+        playSound(name : "a-4")
+    }
+    
+    
 }
+
 
